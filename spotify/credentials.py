b750b59941e0b61e93325a5f3d03a2d7ea772bb3
Ayush Patel <55711453+ayushpatel996@users.noreply.github.com>
2021-07-06 13:24:15 +0530

    Spotify : authenticaton and authorizantion of the user

---
+++ spotify/credentials.py
@@ -0,0 +1,4 @@
+#This is where we store spotify credentials
+CLIENT_ID= "8410b21907784e9091e701139ced6966"
+CLIENT_SECRET = "e5f06d1193f7434885c64f4e6dfc3e85"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
